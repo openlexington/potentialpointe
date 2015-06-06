b760130dcd7ad0abbdca2abc5e70accd9b36f55f
Leslie A. Hensley <hensleyl@papermountain.org>
2015-06-06 14:14:59 -0400

    Fix a few syntax errors in haml

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,4 @@
 
 ActiveRecord::Schema.define(version: 20150606175211) do
 
-  create_table "image", force: true do |t|
-    t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
